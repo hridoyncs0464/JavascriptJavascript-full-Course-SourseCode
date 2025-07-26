a883dae93c4123612812213b29bae3d157e84204
Hridoy <hridoyncs0464@gmail.com>
2025-07-26 21:16:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,134 @@
+// /*console.log(`hellow`)
+ 
+// console.log(`hellow world`)
+// // window.alert(`what have you done`)
+// // window.alert(`this is not right`)
+
+// //comment
+// /*this is a c muktiple line comment
+// */
+
+
+// document.getElementById(`myh1`).textContent=` bangladesh`;
+
+// document.getElementById(`myp`).textContent=` i am a  cse student frome bangladesh`
+
+
+
+
+
+
+// // chapter 1- variable
+// let x;
+// let y;
+// x = 100;
+// y = 50;
+// let sum = x + y;
+// console.log(x);
+// console.log(sum);
+
+
+
+
+// // Data types
+
+// let age = 25;    // number type data type
+// console.log(age); // number type data type
+// let price = 10.99; // number type data type
+//  let gpa  = 4.37; // number type data type
+  
+
+//  console.log(price);
+//  console.log(gpa)
+
+//  console.log(`your are ${age} years old`); 
+// console.log(`the price is TK-${price}`);
+// console.log(`your gpa is :${gpa}`);
+
+
+// // string type = a string is a serise of carectare
+
+// let firstname= " hridoy";
+// let  favfood = "biriany";
+// let addEmail = "hridoyncs0464@gmail.com";
+// console.log(typeof firstname);
+// console.log(`your name is ${firstname}`);
+// console.log(`you lile ${favfood}`);
+// console.log(` youe enail is ${addEmail}`);
+
+
+// // boolean  data type
+
+
+// let online = true;
+// console.log(` hridoy is online ${online}`);
+// let forSale = false;
+// console.log(`is this car is for sale : ${forSale}`);
+
+// let isStudent = true;
+// console.log(` you are a student : ${isStudent}`);
+   
+
+let fulNm = "rashedul islam hridoy";
+let  age = 24;
+let student = true;
+
+// document.getElementById("p1").textContent= fulNm;
+// document.getElementById("p2").textContent= age;
+// document.getElementById("p3").textContent= student;
+
+document.getElementById("p1").textContent = `your name is ${fulNm}`;
+document.getElementById("p2").textContent = `you are ${age} years old`;
+
+document.getElementById("p3").textContent= ` you are a student and this is : ${student}`;
+
+
+// arithmetic operator   
+//  operands( values, variables, etc.)
+
+
+let students = 30;
+students = students + 1;
+console.log(students);
+
+students = students - 1;
+console.log(students);
+
+students = students * 11;
+console.log(students);
+
+students = students /10 ;
+console.log(students);
+
+students = students ** 2; // ** is the  to the power sing
+console.log(students);
+
+  let ExtraStudents = students % 4; // modulop operator 
+   // ==1083 / 4 = 272 and the remainder = 1 
+
+console.log(ExtraStudents); 
+
+
+ 
+//augmented  assignment operators (+=, -=, /+, *=....) 
+
+students += 1; // 1089 + 1 = 1089 or students++: increment
+students -= 1; // 1089 - 1 = 1088 or stuents--; decrement opt
+
+students *= 2; // 1089  * 2 = 2178
+students /= 2; // 1089 + 1 = 544
+ 
+
+
+// operaotr precedence 
+
+/* Operator Precedence 
+  1. Parenthesis()
+  2.exponents
+  3.multiplecation (*) & division(/) & modulo(%)
+  4.addition(+) & substraction(-)
+
+  so, summary :" " >  > * > / > % > + > -
+
+  */
+ 
